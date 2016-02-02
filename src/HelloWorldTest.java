598c7ead435fbc129de134ee9fbb500fc8a6aacd
Ronald Gavino <ronald.gavino@pasker.co>
2016-02-02 11:24:59 +0000

    asdf

--- src/HelloWorldTest.java
+++ src/HelloWorldTest.java
@@ -6,13 +6,13 @@ public class HelloWorldTest extends junit.framework.TestCase {
     	HelloWorld robot = new HelloWorld();
     	String name = "Ronnie";
 
-    	assertEquals("Hello "'" + name, robot.sayHello(name));
+    	assertEquals("Hello " + name, robot.sayHello(name));
     }
 
     public void testSaysHeyBudIfNoName() {
     	HelloWorld robot = new HelloWorld();
 
-    	assertEquals("Hey bud!"'", robot.sayHello());
+    	assertEquals("Hey bud!", robot.sayHello());
     }
     
 }
