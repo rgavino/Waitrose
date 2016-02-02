045eb40d7ac52dbac5ab9e2181779a5997415fc7
Ronald Gavino <ronald.gavino@pasker.co>
2016-02-02 10:28:13 +0000

    asfd

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -3,6 +3,17 @@ package oata;
 import org.apache.log4j.Logger;
 import org.apache.log4j.BasicConfigurator;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+ 
+import org.custommonkey.xmlunit.DetailedDiff;
+import org.custommonkey.xmlunit.Diff;
+import org.custommonkey.xmlunit.Difference;
+import org.xml.sax.SAXException;
+ 
 public class HelloWorld {
     static Logger logger = Logger.getLogger(HelloWorld.class);
 
@@ -10,4 +21,41 @@ public class HelloWorld {
         BasicConfigurator.configure();
         logger.info("Hello World");          // the old SysO-statement
     }
+}
+
+public class ComparisonTest {
+ 
+    public static void main(String[] args) {
+        URL url1 = ComparisonTest.class.getResource("reference.xml");
+        URL url2 = ComparisonTest.class.getResource("comparison.xml");
+        FileReader fr1 = null;
+        FileReader fr2 = null;
+        try {
+            fr1 = new FileReader(url1.getPath());
+            fr2 = new FileReader(url2.getPath());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+ 
+        try {
+            Diff diff = new Diff(fr1, fr2);
+            System.out.println("Similar? " + diff.similar());
+            System.out.println("Identical? " + diff.identical());
+ 
+            DetailedDiff detDiff = new DetailedDiff(diff);
+            List differences = detDiff.getAllDifferences();
+            for (Object object : differences) {
+                Difference difference = (Difference)object;
+                System.out.println("***********************");
+                System.out.println(difference);
+                System.out.println("***********************");
+            }
+ 
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+ 
 }
